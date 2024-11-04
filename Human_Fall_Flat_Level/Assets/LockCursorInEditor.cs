a63c65907a7269ab53a98726b8d774f852487ec6
qzg <1078714157@qq.com>
2024-11-04 16:09:06 +0000

    11.4
    11.4

---
+++ Human_Fall_Flat_Level/Assets/LockCursorInEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+public class LockCursorInEditor : MonoBehaviour
+{
+    public bool LockCursor = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            Cursor.visible = !Cursor.visible;
+            Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
+#endif
